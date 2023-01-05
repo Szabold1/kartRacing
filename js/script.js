865337a8c14d5c36bf1f0e71f34f4d0460e30b62
Boldizsar Szabo <szboldisz@gmail.com>
2023-01-05 14:25:28 +0100

    Add sticky navigation bar

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/////////////////////////////////////////
+// Sticky navigation
+const heroContainerEl = document.querySelector(".hero-container");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-72px",
+  }
+);
+
+obs.observe(heroContainerEl);
